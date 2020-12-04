9605ec0e2e92d3677918d5c01e547ba9538046c5
dijiflex <dijiflex@gmail.com>
2020-12-04 15:17:27 +0300

    Made final changes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) =>{
-//    console.log(); 
-  res.send(`${req.query.name} is are coding live at mssa`)
+app.get('/', (req, res)=>{
+    
+    res.send(`${req.query.name} is coding`)
 })
-
-app.listen(process.env.PORT || 3000);
+ 
+ app.listen(process.env.PORT || 3000);
